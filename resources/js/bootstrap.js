69990eb939485a7d005b636fee71f04b39337316
Gerald <dasallagerald@gmail.com>
2024-08-29 13:31:30 +0800

    fix: updates

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 window.axios = axios;
 
-import Swal from "sweetalert2";
-window.Swal = Swal;
-
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 const token = localStorage.getItem("authToken");
